1a548c2004629793471cbeb85215143bb39c8ddf
GitJaeMin <55350092+anjm1020@users.noreply.github.com>
2022-03-20 09:59:35 +0900

    feat : Update Main.java

--- src/bibimbap/test/Main.java
+++ src/bibimbap/test/Main.java
@@ -1,5 +1,8 @@
 package bibimbap.test;
 
+import bibimbap.test.domain.Item;
+import bibimbap.test.domain.ItemStore;
+
 public class Main {
     public static void main(String[] args) {
 //        1. str 이라는 변수에 자신의 이름을 담아서 출력해주세요
@@ -8,5 +11,26 @@ public class Main {
 //        2. item 클래스를 완성해주세요
 //        3. itemStore 클래스를 완성해주세요
 
+        System.out.println("itemStore 생성");
+        ItemStore store = new ItemStore();
+        store.printItemList();
+        System.out.println();
+
+        System.out.println("addItem");
+        store.addItem(new Item("Coffee", 1500, 10));
+        store.addItem(new Item("IceCream", 500, 5));
+        store.addItem(new Item("Snack", 1000, 20));
+        store.printItemList();
+        System.out.println();
+
+        System.out.println("sellItem");
+        store.sellItem(0, 5);
+        store.printItemList();
+        System.out.println();
+
+        System.out.println("fillItem");
+        store.fillItem(2, 10);
+        store.printItemList();
+        System.out.println();
     }
 }
